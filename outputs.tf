40c9bd88055bc76cebbf48bc3747e17a22a1f09d
opzarjun <arjun.y@opz0.io>
2023-11-02 16:04:34 +0530

    add github action and update module

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "id" {
-  value       = join("", azurerm_network_security_group.nsg.*.id)
+  value       = join("", azurerm_network_security_group.nsg[*].id)
   description = "The network security group configuration ID."
 }
 
